7d8f78d866bdc62fb5cdfdf2d8eaeb5c5ac4cedc
Gineeesss <ggutierrezs01@iescastelar.com>
2023-11-27 20:58:18 +0100

    27-11-23 ejercicios 3,4 array hechos

--- EjemArray1/Array3/src/Main.java
+++ EjemArray1/Array3/src/Main.java
@@ -2,21 +2,38 @@ public class Main {
     public static void main(String[] args) {
       int [][]numeros = new int [4][5];
       int a = numeros.length;
-        System.out.println(a);
-        int sumafilas = 0;
+      System.out.println(a);
+      int sumafilas = 0;
+      int sumatotal = 0;
+
+      //bucle para crear la matriz
       for (int i=0; i<4;i++){
           for (int j=0;j<5;j++) {
               numeros[i][j] = (int) (Math.random() * 100 + 1);
           }
       }
+      //bucle para imprimir la tabla y la columnas extra con la suma de cada fila
         for (int i=0; i<4;i++){
             for (int j=0;j<5;j++) {
-                System.out.print(numeros[i][j]+" ");
+                System.out.print(numeros[i][j]+"  ");
                 sumafilas = sumafilas + numeros[i][j];
             }
             System.out.println("| "+sumafilas);
+            sumatotal = sumafilas + sumatotal;
             sumafilas = 0;
-            System.out.println("b");
         }
+        System.out.println("-------------------------");
+
+        //bucle para imprimir la fila de las sumas de las columnas y la suma total
+        int sumacolumnas = 0;
+        for (int w=0;w<5;w++) {
+            for (int t = 0; t < 4; t++) {
+                sumacolumnas = sumacolumnas + numeros[t][w];
+            }
+            System.out.print(sumacolumnas+" ");
+            sumacolumnas = 0;
+        }
+        System.out.print("| "+sumatotal);
+
     }
 }
--- EjemArray1/Array4/src/Main.java
+++ EjemArray1/Array4/src/Main.java
@@ -1,27 +1,33 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        // Declaración e inicialización del array tridimensional
-        int[][][] numeros = new int[3][3][3];
-
-        // Inicialización de los valores del array tridimensional
-        int num = 1;
-        for (int i = 0; i < numeros.length; i++) {
-            for (int j = 0; j < numeros[i].length; j++) {
-                for (int k = 0; k < numeros[i][j].length; k++) {
-                    numeros[i][j][k] = num++;
+        String[] paises = {"España   ","Rusia    ","Japón    ","Australia"};
+        float[][] medidas = new float [4][10];
+        float media = 0;
+        float alturamaxima=medidas[0][0];
+        float alturaminima=210;
+        float sumamedidas = 0;
+        System.out.println("Pais       |Altura Media      |Altura Máxima        |Altura Mínima");
+        System.out.println("---------------------------------------------------------------");
+        for (int j=0; j<4; j++){
+            for (int i=0;i<10;i++){
+                medidas [j][i]=(float)(Math.random()*70f + 140f);
+                sumamedidas = sumamedidas + medidas[j][i];
+                if (medidas[j][i] > alturamaxima){
+                    alturamaxima = medidas[j][i];
+                }
+                if (medidas[j][i] < alturaminima){
+                    alturaminima = medidas[j][i];
                 }
             }
+            media = Math.round(sumamedidas/medidas[0].length);
+            System.out.println(paises[j] +"      "+ media+"            "+alturamaxima+"            "+alturaminima);
+            alturaminima=210;
+            alturamaxima =0;
+
         }
 
-        // Mostrar los valores almacenados en el array tridimensional
-        for (int i = 0; i < numeros.length; i++) {
-            for (int j = 0; j < numeros[i].length; j++) {
-                for (int k = 0; k < numeros[i][j].length; k++) {
-                    System.out.print(numeros[i][j][k] + " ");
-                }
-                System.out.println();  // Nueva línea después de cada fila
-            }
-            System.out.println();  // Nueva línea después de cada "capa"
-        }
+
     }
 }
