981afc7a24e84bbbab9d27df2d216aed0433a6f0
Gineeesss <ggutierrezs01@iescastelar.com>
2023-11-29 17:32:54 +0100

    29-11-23 ejercicios array maquillados

--- EjemArray1/Array4/src/Main.java
+++ EjemArray1/Array4/src/Main.java
@@ -8,11 +8,17 @@ public class Main {
         float alturamaxima=medidas[0][0];
         float alturaminima=210;
         float sumamedidas = 0;
-        System.out.println("Pais       |Altura Media      |Altura Máxima        |Altura Mínima");
-        System.out.println("---------------------------------------------------------------");
+        System.out.println("________________________________________________________________________________________________________________");
+
+        System.out.printf("|Pais%-7s| Datos%-45s| Altura Media | Altura Máxima | Altura Mínima |\n","","");
+        System.out.println("|___________|___________________________________________________|______________|_______________|_______________|");
         for (int j=0; j<4; j++){
+            System.out.printf("|%-11s| {",paises[j]);
             for (int i=0;i<10;i++){
                 medidas [j][i]=(float)(Math.random()*70f + 140f);
+                if (medidas[j][i]==medidas[j][medidas[0].length-1]){
+                    System.out.printf("%.0f}|",medidas[j][i]);
+                }else System.out.printf("%.0f, ",medidas[j][i]);
                 sumamedidas = sumamedidas + medidas[j][i];
                 if (medidas[j][i] > alturamaxima){
                     alturamaxima = medidas[j][i];
@@ -22,14 +28,18 @@ public class Main {
                 }
             }
             media = (sumamedidas/medidas[0].length);
-           System.out.println(paises[j] +"      "+ Math.round(media)+"            "+alturamaxima+"            "+alturaminima);
-
+           //System.out.println(Math.round(media)+"            "+alturamaxima+"            "+alturaminima);
+            System.out.printf("%9s%6s%9.0f%7s%9.0f%7s\n",Math.round(media),"|",alturamaxima,"|",alturaminima,"|");
             //System.out.printf("%-14s %.0f %-14s %.2f %-14s %.2f\n", paises[j], media,"", alturamaxima,"", alturaminima);
+           // System.out.println("|--------------------------------------------------------------------------------------------------------------|");
 
             alturaminima=210;
             alturamaxima =0;
             sumamedidas = 0;
         }
+    //    System.out.println("|-----------|---------------------------------------------------|--------------|---------------|---------------|");
+
+        System.out.println("|___________|___________________________________________________|______________|_______________|_______________|");
 
 
     }
