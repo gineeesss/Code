36f2f45c909953c625c9f4c1bc0dd84dbdf91612
gines <ggutierrezs01@iescastelar.com>
2023-12-11 12:26:06 +0100

    sorpresa

---
+++ Sorpresa/src/mesas/Mesa.java
@@ -0,0 +1,15 @@
+package mesas;
+public class Mesa {
+    //propiedades
+     int[][] mesas = new int[2][10];
+    //metodos
+    public Mesa(int[][] mesas) { //construye las mesas con cada numero (1-10) y comensales aleatorios(0-5)
+        for(int j=0;j < mesas.length; j++) {
+            for (int i = 0; i < mesas[0].length; i++) {
+                if(j==0){
+                    this.mesas[j][i] = i;
+                }else this.mesas[j][i] = (int)(Math.random()*5+0);
+            }
+         }
+    }
+}
