f7aff6a76b8b39eaa7a66ffb89d94697d3ebd20e
gines <ggutierrezs01@iescastelar.com>
2024-01-10 14:14:44 +0100

    cuenta

--- Cuenta/CuentaMio/src/Main.java
+++ Cuenta/CuentaMio/src/Main.java
@@ -8,61 +8,122 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
-        float[][] vector = new float[2][10];
-
+        int eleccion;
+        //int contador = 0; // cuenta el numero de cuentas que hay creadas  (contaba)
+//        boolean existecuenta;
+        int numeroCuenta;
+        float saldo;
         // Cuenta cuenta ;
         Cuenta[] cuenta =new Cuenta[10];
 
 
         System.out.println("BIENVENIDO/A AL BANCO CASTELAR");
-        System.out.println("[0] NUEVO CLIENTE (crear nueva cuenta)");
-        System.out.println("[1] CONSULTAR SALDO");
-        System.out.println("[2] INGRESAR DINERO");
-        System.out.println("[4] RETIRAR DINERO");
-        System.out.println("[5] CONSULTAR MOROSIDAD");
-        System.out.println("[6] SALIR");
-        int eleccion;
-        int contador =0; // cuenta el numero de cuentas que hay creadas
-        boolean existecuenta;
+        System.out.println("[1] NUEVO CLIENTE (SIN saldo)");
+        System.out.println("[2] NUEVO CLIENTE (CON saldo)");
+        System.out.println("[3] CONSULTAR SALDO");
+        System.out.println("[4] INGRESAR DINERO");
+        System.out.println("[5] RETIRAR DINERO");
+        System.out.println("[6] CONSULTAR MOROSIDAD");
+        System.out.println("[9] SALIR");
+
         do {
             eleccion = scr.nextInt();
-            int numeroCuenta = scr.nextInt();
             switch (eleccion) {
-                case 0: //crear nueva cuenta
-                    existecuenta = false;
+                case 1: //crear nueva cuenta
                     System.out.println("introduce numero de cuenta");
-                    numeroCuenta  = scr.nextInt();
-                    for (int i=0;i<contador && !existecuenta;i++){
+                 //   existecuenta = false;
+                    numeroCuenta = scr.nextInt();
+                    /*for (int i=0;i<Cuenta.getTotalCuentas() && !existecuenta;i++){
                         if(numeroCuenta==cuenta[i].getIdentificador()){
                             existecuenta = true;
-                            System.out.println("Ese numero de cuenta ya existe, introduce una nueva");
-                        }
+                            System.out.println("Ese numero de cuenta ya existe");
+                        }*/
+                    buscarCuenta(numeroCuenta,cuenta);
                     } if (!existecuenta) {
-                        cuenta[0] = new Cuenta(numeroCuenta);
-                        contador ++;
+                        cuenta[Cuenta.getTotalCuentas()] = new Cuenta(numeroCuenta);
                     }
                     break;
 
-                case 1: //cuenta con saldo
+                case 2: //cuenta con saldo
+                    existecuenta = false;
                     System.out.println("Introduce el numero de la cuenta (3 cifras):");
                     numeroCuenta = scr.nextInt();
-                    System.out.println("¿Con cuánto dinero vas a abrir la cuenta?");
-                    float saldo = scr.nextFloat();
-                    cuenta[contador] = new Cuenta(numeroCuenta, saldo);
-                    contador ++;
-                case 2: // consultar saldo de una cuenta
+                    for (int i=0;i<Cuenta.getTotalCuentas() && !existecuenta;i++){
+                        if(numeroCuenta==cuenta[i].getIdentificador()){
+                            existecuenta = true;
+                            System.out.println("Ese numero de cuenta ya existe");
+                        }
+                    } if (!existecuenta) {
+                        System.out.println("¿Con cuánto dinero vas a abrir la cuenta?");
+                        saldo = scr.nextFloat();
+                        cuenta[Cuenta.getTotalCuentas()] = new Cuenta(numeroCuenta, saldo);
+                    }
+                    break;
+
+                case 3: // consultar saldo de una cuenta
                     existecuenta = false;
                     System.out.println("Introduzca el numero de cuenta que quiere consultar");
                     numeroCuenta = scr.nextInt();
-                    for (int i=0;i<contador && !existecuenta;i++){
+                    for (int i=0;i<Cuenta.getTotalCuentas() && !existecuenta;i++){
                         if(numeroCuenta==cuenta[i].getIdentificador()){
                             existecuenta = true;
                             System.out.printf("El saldo de la cuenta %d es %.2f\n", cuenta[i].getIdentificador(), cuenta[i].getSaldo());
                         }
                     } if (!existecuenta) System.out.println("Lo sentimos pero esa cueta no existe");
-                    System.out.printf("El saldo de lacuenta %d es %f\n", cuenta[0].getIdentificador(), cuenta[0].getSaldo());
+                    break;
+                case 4:
+                    existecuenta = false;
+                    System.out.println("Introduzca el numero de cuenta en la que quiere ingresar dinero");
+                    numeroCuenta = scr.nextInt();
+                    for (int i=0;i<Cuenta.getTotalCuentas() && !existecuenta;i++){
+                        if(numeroCuenta==cuenta[i].getIdentificador()){
+                            existecuenta = true;
+                            System.out.println("¿Cuánto dinero quiere ingresar en la cuenta?");
+                            saldo = cuenta[i].ingresar(scr.nextFloat());
+                            System.out.printf("El nuevo saldo de la cuenta es %.2f\n",saldo); // cuenta[i].getSaldo()  si se usase el getter, sería redundante guardar el valor anterior en una variable (saldo)
+                        }
+                    } if (!existecuenta) System.out.println("Lo sentimos pero esa cueta no existe");
+                    break;
+                case 5:
+                    existecuenta = false;
+                    System.out.println("Introduzca el numero de cuenta en la que quiere retirar");
+                    numeroCuenta = scr.nextInt();
+                    for (int i=0;i<Cuenta.getTotalCuentas() && !existecuenta;i++){
+                        if(numeroCuenta==cuenta[i].getIdentificador()){
+                            existecuenta = true;
+                            System.out.println("¿Cuánto dinero quiere retirar en la cuenta?");
+                            saldo = cuenta[i].retirar(scr.nextFloat());
+                            System.out.printf("El nuevo saldo de la cuenta es %.2f\n",saldo); // cuenta[i].getSaldo()  si se usase el getter, sería redundante guardar el valor anterior en una variable (saldo)
+                        }
+                    } if (!existecuenta) System.out.println("Lo sentimos pero esa cueta no existe");
+                    break;
+                case 6:
+                    existecuenta = false;
+                    System.out.println("Introduzca el numero de cuenta para comprobar");
+                    numeroCuenta = scr.nextInt();
+                    for (int i=0;i<Cuenta.getTotalCuentas() && !existecuenta;i++){
+                        if(numeroCuenta==cuenta[i].getIdentificador()){
+                            existecuenta = true;
+                            if(cuenta[i].esMorosa()){
+                                System.out.println("Esta cuenta SI es morosa" + cuenta[i].getSaldo());
+                            }else System.out.println("Esta cuenta NO es morosa" + cuenta[i].getSaldo());
+                        }
+                    } if (!existecuenta) System.out.println("Lo sentimos pero esa cueta no existe");
+                    break;
+                case 7:
+                    System.out.printf("En nuestro  banco hayun total de %d cuentas\n", Cuenta.getTotalCuentas());
+                    System.out.printf("El dinero total que hay en el banco es %.2f €\n", Cuenta.getTotalSaldo());
                     break;
             }
-        }while(eleccion != 6);
+        }while(eleccion != 9);
+    }
+    private static void buscarCuenta(int numeroCuenta, Cuenta[] cuenta){
+    boolean existecuenta = false;
+        for (int i=0;i<Cuenta.getTotalCuentas() && !existecuenta;i++){
+            if(numeroCuenta==cuenta[i].getIdentificador()){
+                existecuenta = true;
+                System.out.println("Ese numero de cuenta ya existe, introduce una nueva");
+            }
+        }
     }
 }
--- Cuenta/CuentaMio/src/banco/Cuenta.java
+++ Cuenta/CuentaMio/src/banco/Cuenta.java
@@ -4,36 +4,49 @@ import java.util.Scanner;
 
 public class Cuenta {
     //PROPERTIES
-    int identificador;
-    float saldo;
+    private int identificador;
+    private float saldo;
+    private static int totalCuentas;
+    private static float totalSaldo;
 
     //METHODS
-
     public  Cuenta(int numCuenta){
         this.identificador=numCuenta;
         this.saldo=0;
+        totalCuentas++;
     }
     public Cuenta(int numCuenta, float saldo){
         this.identificador=numCuenta;
         this.saldo=saldo;
+        totalCuentas++;
+        totalSaldo+=saldo;
     }
-    public float getSaldo() {
-        return saldo;
-    }
-    public int getIdentificador() {
-        return identificador;
-    }
+    public float getSaldo() {return saldo;}
+    public int getIdentificador() {return identificador;}
+
+    public static int getTotalCuentas() {return totalCuentas;}
+
+    public static float getTotalSaldo() {return totalSaldo;}
 
     public float ingresar(float importe){
-        return this.saldo += importe;
+        this.saldo += importe;
+        totalSaldo+=importe;
+        return this.saldo;
     }
     public float retirar(float importe){
         Scanner scr = new Scanner(System.in);
         if (this.saldo - importe < 0){
             System.out.println("El balance de la cuenta quedará en negativo, ¿desea continuar? [S/N]");
-            if(scr.nextLine().toLowerCase().equals("s")) this.saldo -= importe; // se puedde usar .equalsIgnoreCase()
-        }else this.saldo -=importe;
-        return this.saldo -= importe;
+        //    if(scr.nextLine().toLowerCase().equals("s")) this.saldo -= importe; // se puedde usar .equalsIgnoreCase()
+            if (scr.nextLine().equalsIgnoreCase("s")){
+                this.saldo=this.saldo - importe;
+            }
+        }else {
+            this.saldo -=importe;
+            totalSaldo-=importe;
+        }
+        this.saldo -= importe;
+        return this.saldo;
     }
     public boolean esMorosa(){
         boolean morosa = false;
