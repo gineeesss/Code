69d0238cd4382a728e168ec2d100bbc0948f2483
Gineeesss <ggutierrezs01@iescastelar.com>
2024-01-17 23:16:30 +0100

    a

--- PracticaJar/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Conexion conn = new Conexion();
-        String sql = "SELECT * FROM personas;";
-        conn.select( sql );
-    }
-}
